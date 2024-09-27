4f35479e38f80bbc849d7814b4ac9ef85d193101
Mr STRATEGIST <142794511+tandavbot2@users.noreply.github.com>
2024-09-27 13:59:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config(object):
     API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH"))
-    BOT_TOKEN = os.environ.get("BOT_TOKEN"))
-
-    # Add sudo users as a list of user IDs
-    SUDO_USERS = [6265954306, 7169824974]  # Replace with actual user IDs
+    API_HASH = os.environ.get("API_HASH")  # Removed the extra closing parenthesis
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    SUDO_USERS = [int(user) for user in os.environ.get("SUDO_USERS", "").split()]
+    
